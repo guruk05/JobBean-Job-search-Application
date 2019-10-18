57c5e9e53b4cfccda45ae81660f6cc077b446f8f
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-18 22:52:04 +0530

    created React app

---
+++ job/src/App.js
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+
+
+
+
+const App = () => {
+  
+  const url = "https://nut-case.s3.amazonaws.com/jobs.json";
+   
+  // const [ , setJobs] = useState([]);
+ 
+ const getData = async() =>{
+  const jobData = await fetch(url);
+  const data = await jobData.json();
+  console.log(data);
+  // console.log(data.data[0].title);
+
+}
+
+getData();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Job Search
+      </header>
+      <p>Search Jobs Here</p>
+      <form className = "search-Jobs">
+        <input className = "search-Form" type ="text" name="searchJob"/>
+        <button className = "submit-Button"> Submit </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
