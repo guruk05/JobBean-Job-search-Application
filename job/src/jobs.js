07705b56e240b8bcafa89a93cc1bbd9ff4f00c29
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-19 21:29:15 +0530

    deconstructing data

--- job/src/jobs.js
+++ job/src/jobs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-const Job = ({title, companyName, location, experience, skills, type, endDate}) =>{
+const Job = ({title, companyName, location, experience, skills, type, endDate, apply}) =>{
 
 
     return (
