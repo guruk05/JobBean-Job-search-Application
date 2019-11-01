8214c6b8374e0a7cf5653eaffb46b52306b68dc1
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-01 21:26:56 +0530

    Implementing filter

--- job/src/App.js
+++ job/src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Job from './Jobs';
 import './App.css';
+import { Button } from 'reactstrap';
 
 
 
@@ -10,32 +11,17 @@ const App = () => {
   const url = "https://nut-case.s3.amazonaws.com/jobs.json";
    
   const [jobs , setJobs] = useState([]);
-  const [filteredJobs , setFilteredJobs] = useState("");
-  const [filterResults , setFilteredResults] = useState('Bangalore');
-  // console.log(jobs);
+  
  
   useEffect(() => {
     getData();
-  }, [filterResults]); 
-
-  const getSearch = (e) => {
-    setFilteredJobs(e.target.value);
-  };
-
-  const submitFilter = (e) => {
-    e.preventDefault()
-    setFilteredResults(filteredJobs)
-  }
+  }, []); 
 
  const getData = async() =>{
   const jobData = await fetch(url);
   const data = await jobData.json();
   setJobs(data.data);
-  // console.log(data.data);
-}
-
-
-
+}  
 
   return (
     <div className="App">
@@ -44,11 +30,11 @@ const App = () => {
       </header>
       <p>Search Jobs Here</p>
       <form className = "search-Jobs">
-        <input className = "search-Form" type ="text" name="searchJob" value={filteredJobs} onChange = {getSearch}/>
-        <button className = "submit-Button"> Submit </button>
+        <input className = "search-Form" type ="text" name="searchJob"/>
+        <Button outline color="secondary">Search</Button>
       </form>
       {jobs.map(jobData => (
-         <Job key = {jobData.title}
+         <Job key = {jobData.companyName}
               title = {jobData.title} 
               companyName = {jobData.companyname} 
               location = {jobData.location} 
--- job/src/jobs.js
+++ job/src/jobs.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 
 const Job = ({title, companyName, location, experience, skills, type, endDate, apply}) =>{
-
-
     return (
          <div className = "card">
-            <div className = "title"> <h2>{title}</h2> </div> 
+            <div className = "title"><h2>{title}</h2> </div> 
             <div className = "companyName"> <h4>{companyName}</h4></div>
             <div className = "location"> <p>{location}</p></div>
             <div className = "experience"> <p>{experience}</p></div>
@@ -14,11 +12,9 @@ const Job = ({title, companyName, location, experience, skills, type, endDate, a
             <div className = "type"> <p>{type}</p></div>
             <div className = "endDate"><p>{endDate}</p></div>
             <div className = "apply"><p>{apply}</p></div>
-        </div>  
-    );
-}
-
-
+        </div>
+    )
+};
 
 
 export default Job;
