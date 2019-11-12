3422061bd28d5e1382e7b4f98a14e2817a5bf8c8
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-11-12 15:23:29 +0530

    return a nav tag

--- job/src/pagination.js
+++ job/src/pagination.js
@@ -8,4 +8,22 @@ const Pagination = ({paginate, }) => {
         } 
     }
 
-   
+    return (
+        <nav>
+            <ul className="pagination">
+                {pageNumbers.map(number => {
+                    return(
+                        <li className="page-item">
+                        <a onClick = {() => paginate(number)} href = "!#" className = "page-link">
+                            {number} 
+                        </a>
+                    </li>
+                    )
+                })}
+            </ul>
+        </nav>
+    )
+
+
+
+export default Pagination;
